e50a8a438e3eeb79f19f721b8f113558f9b6dc5c
Markus Kobler <markus@distinctive.co>
2016-09-10 14:01:48 +0100

    Fixing path

--- build.sh
+++ build.sh
@@ -8,11 +8,11 @@ out=${PWD}/out
 
 go build -o ${out}/gstore ${src}/main.go
 
-cp ${src}/upload.sh ${out}/upload.sh
+cp ${src}/override.sh ${out}/override.sh
 
 cat <<EOF > ${out}/Dockerfile
 FROM quay.io/desource/acbuild-resource
 
 COPY gstore     /opt/bin/gstore
-COPY upload.sh  /opt/resource/upload.sh
+COPY override.sh  /opt/resource/override.sh
 EOF
