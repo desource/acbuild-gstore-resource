0b38575227ac09e5e617509f233e3f0fe7a65907
Markus Kobler <markus@distinctive.co>
2016-09-10 20:35:00 +0100

    Updating override script to take optional args

--- override.sh
+++ override.sh
@@ -4,31 +4,36 @@ upload() {
     destination=$1
     payload=$2
 
-    bucket=$(jq -r '.source.bucket // ""' < $payload)
-    email=$(jq -r '.source.email // ""' < $payload)
-
-    private_key_file=$(mktemp /tmp/private-key-file.XXXXXX)
-    
-    set -x
-
+    private_key_file=$(mktemp /tmp/private-key-file.XXXXXX)    
     cat <<EOF > $private_key_file
 $(jq -r '.source.private_key // ""' < $payload)
 EOF
+    args="-privateKey=${private_key_file}"
+    
+    email=$(jq -r '.source.email // ""' < $payload)
+    if [ ! -z "${email}" ]; then
+        args="${args} -email=${email}"
+    fi
+    
+    bucket=$(jq -r '.source.bucket // ""' < $payload)
+    if [ ! -z "${bucket}" ]; then
+        args="${args} -bucket=${bucket}"
+    fi
 
     prefix=$(jq -r '.params.prefix // ""' < $payload)
+    if [ ! -z "${prefix}" ]; then
+        args="${args} -prefix=${prefix}"
+    fi
+ 
     dir=$(jq -r '.params.dir // ""' < $payload)
+    if [ ! -z "${dir}" ]; then
+        args="${args} ${dir}"
+    fi
+   
     pwd=$(jq -r '.params.pwd // ""' < $payload)
-
     if [  -n "$pwd" ]; then
         cd $pwd
     fi
-
-    cat $private_key_file
-
-    /opt/bin/gstore \
-        -bucket $bucket \
-        -prefix $prefix \
-        -email $email \
-        -privateKey $private_key_file \
-        $dir
+    
+    eval /opt/bin/gstore ${args}
 }
