07b72fc5059474b5819e069814222e08b8350435
Markus Kobler <markus@distinctive.co>
2016-09-10 16:22:33 +0100

    Build static container

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ export GOPATH=${PWD}/go
 src=${PWD}/go/src/github.com/desource/acbuild-gstore-resource
 out=${PWD}/out
 
-go build -o ${out}/gstore ${src}/main.go
+CGO_ENABLED=0 go build -o ${out}/gstore ${src}/main.go
 
 cp ${src}/override.sh ${out}/override.sh
 
